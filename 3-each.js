f7ac0210ca11df38eb1b56aace2fdd0be0ab3b5c
Theron <colterthehammer@gmail.com>
2018-03-05 13:38:31 -0700

    Times

---
+++ 3-each.js
@@ -0,0 +1,15 @@
+/** 3-each
+ * 1. receive two URLs as the first and second cmd args
+ * 2. http.get, create two requests, console.log any errors
+ */
+
+const async = require('async');
+const http = require('http');
+
+async.each([process.argv[2], process.argv[3]], (item, done) => {
+    http.get(item, res => {
+        res.on('error', err => done(err));
+    })
+}, (err, results) => {
+    if (err) console.log(err);
+})
